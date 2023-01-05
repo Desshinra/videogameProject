a2b350b40e44d4bae3148d2f5f838d49857482d8
Amado Dubon <desshinra@gmail.com>
2023-01-05 14:28:41 -0600

    Add: Scale of canvas and items

--- game.js
+++ game.js
@@ -2,9 +2,43 @@ const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
 window.addEventListener('load', startGame)
+window.addEventListener('resize', startGame)
+
+let canvasScale;
+let elementsScale = canvasScale / 10;
 
 function startGame() {
+    setCanvasScale()
+
+    console.log({canvasScale, elementsScale});
+    game.font = elementsScale + 'px Verdana';
+    game.textAlign = 'end';
+    // game.fillText(emojis['PLAYER'], elementsScale * 2, elementsScale * 2.5);
+    
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementsScale, elementsScale * i);
+    }
+
+    // window.innerWidth
+    // window.innerHeight
     // game.
-    game.fillRect(0,0,100,100);
-    game.clearRect(0,0,50,50);
+    // game.fillRect(0,0,100,100);
+    // game.clearRect(0,0,50,50);
+    // game.font = "25px Verdana";
+    // game.fillStyle = 'purple';
+    // game.textAlign = '';
+    // game.fillText('Platzi');
+}
+
+function setCanvasScale() {
+
+    if(window.innerHeight > window.innerWidth) {
+        canvasScale = window.innerWidth * 0.80;
+    } else {
+        canvasScale = window.innerHeight * 0.80;
+    }
+
+    canvas.setAttribute('width', canvasScale);
+    canvas.setAttribute('height', canvasScale);
+
 }
